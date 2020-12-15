8e761166651a69643412f3047b9b58c0f2c17ce7
plegall <plg@piwigo.org>
2020-12-15 17:20:03 +0100

    clean line endings and indentation

--- main.inc.php
+++ main.inc.php
@@ -2,64 +2,70 @@
 /*
 Plugin Name: rightClick
 Version: auto
-Description: rightClick deactivates the context menu on websized pictures and more. 
+Description: rightClick deactivates the context menu on websized pictures and more.
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=401
 Author: VDigital
 Author URI: http://piwigo.org/
 */
 
 if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
 define('RIGHTCK_DIR' , basename(dirname(__FILE__)));
 define('RIGHTCK_PATH' , PHPWG_PLUGINS_PATH . RIGHTCK_DIR . '/');
 
-if ( !function_exists( 'rightClick_protect' ) )
+if ( defined('IN_ADMIN') and IN_ADMIN )
 {
-	if ( defined('IN_ADMIN') and IN_ADMIN ) return false;
+  return false;
+}
 
-	add_event_handler('loc_after_page_header', 'rightClick_protect', 20);
+add_event_handler('loc_after_page_header', 'rightClick_protect', 20);
 
-	function  rightClick_protect()
+function  rightClick_protect()
+{
+  global $template, $user, $conf;
+
+  $Maxlvl = isset($conf['rightClick_level']) ? $conf['rightClick_level'] : max($conf['available_permission_levels']);
+
+	if ($user['level'] >= $Maxlvl)
   {
-		global $template, $user, $conf;
-		if (!isset($conf['rightClick_level']))
-      $Maxlvl = max($conf['available_permission_levels']);
-		else
-      $Maxlvl = $conf['rightClick_level'];
+    return;
+  }
 
-		if ($user['level'] >= $Maxlvl) return;
+  $default = array(
+    '#theImage img',
+    '#theImage area',
+    '#akPicture',
+    '#colorbox',
+    '#sb-container',
+    '#prevnext',
+    '#theMainImage',
+    '#theImgHigh', // Stripped, zoom on HD
+    '.thumbnailCategory img',
+    '.thumbnails img',
+    '#thumbnails img', // specific on Stripped
+    '#myHomePage img', // Homepage with Additional Pages
+    '.fotorama__img', // Fotorama
+    '.pswp img', // Bootstrap Darkroom
+  );
 
-                $default = array(
-                  '#theImage img',
-                  '#theImage area',
-                  '#akPicture',
-                  '#colorbox',
-                  '#sb-container',
-                  '#prevnext',
-                  '#theMainImage',
-                  '#theImgHigh', // Stripped, zoom on HD
-                  '.thumbnailCategory img',
-                  '.thumbnails img',
-                  '#thumbnails img', // specific on Stripped
-                  '#myHomePage img', // Homepage with Additional Pages
-                  '.fotorama__img', // Fotorama
-                  '.pswp img',      // Bootstrap Darkroom
-                  );
-		if (!isset($conf['rightClick_selectors']))
-      $conf['rightClick_selectors'] = array();
+  if (!isset($conf['rightClick_selectors']))
+  {
+    $conf['rightClick_selectors'] = array();
+  }
 
-		$conf['rightClick_selectors'] = array_unique( array_merge($default, $conf['rightClick_selectors']) );
-		
-		$template->set_filenames(array(
-		'rightClick_protect' => dirname(__FILE__) . '/template/rightClick_protect.tpl',
-		));
-		$Path = embellish_url($template->get_template_vars('ROOT_URL').RIGHTCK_PATH);
-		$protect = array(
-			'Dir' 				=> RIGHTCK_DIR,
-			'Path' 				=> $Path,
-			'Selectors'			=> $conf['rightClick_selectors'],
-			);
-		$template->assign( array( 'RIGHTCK' => $protect ) );
-		$template->parse('rightClick_protect');
-	}
+  $conf['rightClick_selectors'] = array_unique( array_merge($default, $conf['rightClick_selectors']) );
+
+  $template->set_filenames(array('rightClick_protect' => dirname(__FILE__) . '/template/rightClick_protect.tpl'));
+
+  $Path = embellish_url($template->get_template_vars('ROOT_URL').RIGHTCK_PATH);
+
+  $protect = array(
+    'Dir' => RIGHTCK_DIR,
+    'Path' => $Path,
+    'Selectors' => $conf['rightClick_selectors'],
+  );
+
+  $template->assign(array('RIGHTCK' => $protect));
+  $template->parse('rightClick_protect');
 }
 ?>
